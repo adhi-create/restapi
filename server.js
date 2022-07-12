00e6e88981d3e17960766b6f947fc9a3d169b186
adhi <108246061+adhi-create@users.noreply.github.com>
2022-07-12 23:39:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,66 +1,24 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const TaskSchema = require('./model');
-const cors = require('cors');
-
+const express = require('express')
 const app = express();
-
-mongoose.connect('mongodb+srv://seshu1433:seshu1433@cluster0.nzzgh.mongodb.net/?retryWrites=true&w=majority',
-{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(
-    ()=>console.log('DB Connected')
+app.get('/',(req,res) => {
+   res.send('hello')
+})
+const products = [
+    {
+        id:1,
+        name:"mi"
+    },
+    {
+        id:2,
+        name:"iphone"
+    },
+]
+app.get('/products',(req,res)=>{
+    res.json(products)
+})
+app.get('/products/:id',(req,res)=>{
+    const newData = products.filter(item =>item.id.toString() === req.params.id);
+    return res.json(newData)
+},
 )
-
-app.use(express.json())
-
-app.use(cors({
-    origin: '*'
-}))
-
-app.post('/addtask',async(req,res)=>{
-    const {todo} = req.body;
-    try{
-        const newData = new TaskSchema({
-            todo : todo
-        });
-        await newData.save();
-        return res.json(await TaskSchema.find())
-    }
-    catch(err){
-        console.log(err)
-    }
-})
-
-app.get('/gettask',async(req,res) => {
-    try{
-        return res.json(await TaskSchema.find()) ;
-    }
-    catch(err){
-        console.log(err)
-    }
-})
-
-app.delete('/delete/:id',async(req,res) => {
-    try{
-        await TaskSchema.findByIdAndDelete(req.params.id);
-        return res.json(await TaskSchema.find())
-    }
-    catch(err){
-        console.log(err)
-    }
-})
-
-
-app.get('/gettask/:id',async(req,res) => {
-    try{
-        return res.json(await TaskSchema.findById(req.params.id)) ;
-    }
-    catch(err){
-        console.log(err)
-    }
-})
-
-
-app.listen(process.env.PORT,()=> console.log('Server running...'));
+app.listen(5000,()=> console.log('Server running...'));
