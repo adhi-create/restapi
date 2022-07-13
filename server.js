0371e02650a6f135c8190327c088d6c887dd7e45
adhi <108246061+adhi-create@users.noreply.github.com>
2022-07-13 13:32:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 app.get('/',(req,res) => {
    res.send('hello')
 })
-const products = [
+const users = [
     {
         id:1,
         name:"mi"
@@ -13,7 +13,7 @@ const products = [
         name:"iphone"
     },
 ]
-app.get('/products',(req,res)=>{
+app.get('/users',(req,res)=>{
     res.json(products)
 })
 app.get('/products/:id',(req,res)=>{
